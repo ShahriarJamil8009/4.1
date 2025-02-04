9a6ff07869f54af03a58debb6e6d615a1cffa458
Shahriar Jâmil <106110965+ShahriarJamil8009@users.noreply.github.com>
2025-02-04 22:56:14 +0600

    Add files via upload

---
+++ 2. Object Oriented Design and Desing Pattern/Lab/Experiment_1/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Duck obj = new Duck();
+
+        // Calling methods from different interfaces
+        obj.eat();
+        obj.fly();
+        obj.swim();
+    }
+}
