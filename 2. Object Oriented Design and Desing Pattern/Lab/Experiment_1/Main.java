ba91268999aadf14276bb87c19e8df9672fb5059
Shahriar Jâmil <106110965+ShahriarJamil8009@users.noreply.github.com>
2025-02-04 23:05:19 +0600

    Update Main.java

--- 2. Object Oriented Design and Desing Pattern/Lab/Experiment_1/Main.java
+++ 2. Object Oriented Design and Desing Pattern/Lab/Experiment_1/Main.java
@@ -1,3 +1,10 @@
+/* 
+1. Demonstrate Multiple Interface/Inheritance:  
+Write a Java program to demonstrate multiple interface/inheritance.
+You are free to choose any application for showcasing multiple interface/inheritance.
+*/    
+
+
 public class Main {
     public static void main(String[] args) {
         Duck obj = new Duck();
